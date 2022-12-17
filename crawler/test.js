ed5730cc9e8395783dde3c450d22136093647fe5
Cleo <tzuhan1107@gmail.com>
2022-12-17 12:02:11 +0800

    add dotenv

--- crawler/test.js
+++ crawler/test.js
@@ -1,25 +1,25 @@
 // install mysql2
 const mysql2 = require('mysql2/promise');
 
+// 讀設定檔 npm i dotenv
+require('dotenv').config();
+// let dotenv = require('dotenv');
+// dotenv.config();
+
 (async () => {
-    const connection = await mysql2.createConnection({
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'admin',
-        password: '',
-        database: 'stock_mfee31',
-    });
+  console.log(process.env.DB_USER);
+  const connection = await mysql2.createConnection({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    database: process.env.DB_NAME,
+  });
 
-        // simple query
-        // let result = await connection.query('SELECT * FROM `stocks`');
-        // let data = result[0];
-        //
-        let [data, filed] = await connection.query('SELECT * FROM `stocks`');
-        // console.log(result);
-        console.log(data);
+  // simple query
+  let [data, fields] = await connection.query('SELECT * FROM `stocks`');
+  // console.log(result);
+  console.log(data);
 
-        connection.end();
-})()
-
-
-
+  connection.end();
+})();
