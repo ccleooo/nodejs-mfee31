434c2deea78f6b99d22e255677f19caee4696b08
Cleo <tzuhan1107@gmail.com>
2022-12-24 17:17:55 +0800

    update stock-fe

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -1,7 +1,21 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const StockDetails = () => {
   const [error, setError] = useState(null);
+  const { stockId } = useParams();
+
+  const [sata, setData] = useState();
+  // 用[]，component 第一次初始化的時候跑到
+  useEffect(()=>{
+    async function setData() {
+      let response = await axios.get(`http://localhost:3001/api/stocks/${stockId}`);
+      setData(response.data);
+    }
+  }, [])
+
+  console.log('StockDetail', stockId);
 
   return (
     <div>
@@ -25,32 +39,17 @@ const StockDetails = () => {
         </li>
       </ul>
       目前在第 1 頁
+
       <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
         <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：date</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交金額：amount
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交股數： volume
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          開盤價：open_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          收盤價：close_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          漲跌價差：delta_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最高價：high_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最低價：low_price
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交筆數：transactions
-        </h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：amount</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數： volume</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：open_price</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：close_price</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：delta_price</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：high_price</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：low_price</h2>
+        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：transactions</h2>
       </div>
     </div>
   );
