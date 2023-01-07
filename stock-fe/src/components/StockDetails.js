7a0309629d4e926eb248c2bd8f2c6d3448a03c68
Cleo <tzuhan1107@gmail.com>
2023-01-07 14:13:07 +0800

    前端:股價細節頁-頁碼

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -5,52 +5,71 @@ import axios from 'axios';
 const StockDetails = () => {
   const [error, setError] = useState(null);
   const { stockId } = useParams();
+  const [page, setPage] = useState(1); // 目前在哪一頁
+  const [totalPage, setTotalPage] = useState(0); // 總共有幾頁
 
-  const [sata, setData] = useState();
-  // 用[]，component 第一次初始化的時候跑到
-  useEffect(()=>{
-    async function setData() {
-      let response = await axios.get(`http://localhost:3001/api/stocks/${stockId}`);
-      setData(response.data);
+  const [data, setData] = useState([]);
+  // 用 []，component 第一次初始化的時候會跑到
+  useEffect(() => {
+    console.log('page 改變的 useEffect', page);
+    async function getData() {
+      let response = await axios.get(`http://localhost:3001/api/stocks/${stockId}?page=${page}`);
+      setData(response.data.data);
+      setTotalPage(response.data.pagination.totalPage);
     }
-  }, [])
+    getData();
+  }, [page]);
 
-  console.log('StockDetail', stockId);
-
-  return (
-    <div>
-      {error && <div>{error}</div>}
-      <ul>
+  const getPages = () => {
+    let pages = [];
+    for (let i = 1; i <= totalPage; i++){
+      pages.push(
         <li
           style={{
             display: 'inline-block',
             margin: '2px',
-            // backgroundColor: page === i ? '#00d1b2' : '',
-            // borderColor: page === i ? '#00d1b2' : '#dbdbdb',
-            // color: page === i ? '#fff' : '#363636',
+            backgroundColor: page === i ? '#00d1b2' : '',
+            borderColor: page === i ? '#00d1b2' : '#dbdbdb',
+            color: page === i ? '#fff' : '#363636',
             borderWidth: '1px',
             width: '28px',
             height: '28px',
             borderRadius: '3px',
             textAlign: 'center',
           }}
+          key={i}
+          onClick={(e)=>{
+            setPage(i)
+          }}
         >
-          1
+          {i}
         </li>
-      </ul>
-      目前在第 1 頁
+      )
+    }
+    return pages
+    
+  }
 
-      <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：date</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：amount</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數： volume</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：open_price</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：close_price</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：delta_price</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：high_price</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：low_price</h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：transactions</h2>
-      </div>
+  return (
+    <div>
+      {error && <div>{error}</div>}
+      <ul>{getPages()}</ul>
+      目前在第 {page} 頁
+      {data.map((item) => {
+        return (
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6" key={item.date}>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">日期：{item.date}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交金額：{item.amount}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交股數： {item.volume}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">開盤價：{item.open_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">收盤價：{item.close_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">漲跌價差：{item.delta_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">最高價：{item.high_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">最低價：{item.low_price}</h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">成交筆數：{item.transactions}</h2>
+          </div>
+        );
+      })}
     </div>
   );
 };
