190f54fa78261b82f8f7d8509dd7d68345616dac
Cleo <tzuhan1107@gmail.com>
2023-01-07 10:36:59 +0800

    新增一個股票代碼

--- stock-be/server.js
+++ stock-be/server.js
@@ -15,6 +15,11 @@ let pool = mysql2.createPool({
   connectionLimit: 10,
 });
 
+// 如果要讓 express 認得 json 資料
+// request Content-Type: application/json
+// 需要加上這個中間件
+app.use(express.json());
+
 // 允許跨源存取
 // 預設是全部開放
 // 也可以做部分限制，參考 npm cors 的文件
@@ -78,8 +83,18 @@ app.get('/api/stocks', async (req, res, next) => {
 app.get('/api/stocks/:stockId', async (req, res, next) => {
   console.log('/api/stocks/:stockId => ', req.params.stockId);
   // 會用 prepared statement 的方式來避免發生 sql injection
-  let [data] = await pool.query('SELECT * FROM stock_prices WHERE stock_id=?', [req.params.stockId]);
+  // pool.query vs pool.execute
+  let [data] = await pool.execute('SELECT * FROM stock_prices WHERE stock_id=?', [req.params.stockId]);
   res.json(data);
+});
+
+app.post('/api/stocks', async (req, res) => {
+  console.log('POST /api/stocks', req.body);
+  // req.body.stockId, req.body.stockName
+  // 完成 insert
+  let results = await pool.query('INSERT INTO stocks (id, name) VALUES (?, ?);', [req.body.stockId, req.body.stockName]);
+  // console.log('POST stocks results', results);
+  res.json({ result: 'ok' });
 });
 
 app.use((req, res, next) => {
