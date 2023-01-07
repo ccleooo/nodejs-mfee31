ce0bfc0bb7296a3a7c543fa769096dd8e565ea04
Cleo <tzuhan1107@gmail.com>
2023-01-07 16:50:51 +0800

    前端:註冊功能

--- stock-fe/src/components/Register.js
+++ stock-fe/src/components/Register.js
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import axios from 'axios';
+
 const Register = () => {
+
+  const [member, setMember] = useState({
+    email:'tzuhan1107@gmail.com',
+    name: 'cleo',
+    password: '12345',
+    confirmPassword: '12345'
+  });
+
+  // 綁定email input 的 change
+  // e.target ==> email.input
+  // name input 的 change
+  // e.target ==> name input
+  function handleChange(e){
+    // console.log(e)     // 觀察e到底有甚麼
+    // member.email = e.target.value; // 對react來說是錯誤寫法
+    let newMember = {...member}; // 先複製一個新的
+    newMember[e.target.name] = e.target.value; // 改新的記憶體
+    setMember(newMember);
+
+    //  setMember({...member, [e.target.name]: e.target.value}); //上面三行的高級寫法
+  }
+
+  async function handleSubmit(e) {
+    console.log('handleSubmit');
+    // 關閉表單的預設行為
+    e.preventDefault();
+    // ajax
+    let response = await axios.post('http://localhost:3001/api/register',member);
+    console.log(response.data);
+  }
+
+
   return (
     <form className="bg-purple-100 h-screen md:h-full md:my-20 md:mx-16 lg:mx-28 xl:mx-40 py-16 md:py-8 px-24 text-gray-800 md:shadow md:rounded flex flex-col md:justify-center">
       <h2 className="flex justify-center text-3xl mb-6 border-b-2 pb-2 border-gray-300">
@@ -13,6 +48,8 @@ const Register = () => {
           type="text"
           id="email"
           name="email"
+          value={member.email}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-4 text-2xl">
@@ -24,6 +61,9 @@ const Register = () => {
           type="text"
           id="name"
           name="name"
+          value={member.name}
+          onChange={handleChange}
+          
         />
       </div>
       <div className="mb-4 text-2xl">
@@ -35,6 +75,8 @@ const Register = () => {
           type="password"
           id="password"
           name="password"
+          value={member.password}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-8 text-2xl">
@@ -46,6 +88,8 @@ const Register = () => {
           type="password"
           id="confirmPassword"
           name="confirmPassword"
+          value={member.confirmPassword}
+          onChange={handleChange}
         />
       </div>
       <div className="mb-8 text-2xl">
@@ -59,7 +103,7 @@ const Register = () => {
           name="photo"
         />
       </div>
-      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in">
+      <button className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in" onClick={handleSubmit}>
         註冊
       </button>
     </form>
